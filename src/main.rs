11a451d0bdb03c3bd055982a8165fc89d0ce569e
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-03-12 13:20:48 +0530

    feat: fetch page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use scraper::{Html, Selector};
+
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+    let url = String::from("https://solana.com");
+    let html = fetch_page(&url).await?;
+
+
+    Ok(())
+}
+
+async fn fetch_page(url: &String)-> Result<String, Box<dyn  std::error::Error>> { 
+    let resp = reqwest::get(url).await?.text().await?;
+    Ok(resp)
 }
