2359d45d44ecedb01b8399b67ef60871102d218c
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-03-12 13:36:22 +0530

    feat: parse links from webpage

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
     let url = String::from("https://solana.com");
     let html = fetch_page(&url).await?;
-
+    let parsed_links = parse_links(&html);
+    println!("{parsed_links:?}");
 
     Ok(())
 }
@@ -15,4 +16,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 async fn fetch_page(url: &String)-> Result<String, Box<dyn  std::error::Error>> { 
     let resp = reqwest::get(url).await?.text().await?;
     Ok(resp)
+}
+
+fn parse_links(html : &str)-> Vec<String>{
+    let mut links = vec![];
+    let parsed_link = Html::parse_document(html);
+    let selector = Selector::parse("a").unwrap();
+   let elements =  parsed_link.select(&selector);   
+    for element in elements{
+       if let Some(href) = element.value().attr("href"){
+        links.push(href.to_string());
+       }
+    }
+    links
+
 }
